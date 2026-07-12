de81ba591cceca2b86b280373edeb2b23f5f82a4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "vpn_gateway_nat_rules" {
-  description = "All vpn_gateway_nat_rule resources"
-  value       = azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules
-}
 output "vpn_gateway_nat_rules_external_mapping" {
-  description = "List of external_mapping values across all vpn_gateway_nat_rules"
-  value       = [for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : v.external_mapping]
+  description = "Map of external_mapping values across all vpn_gateway_nat_rules, keyed the same as var.vpn_gateway_nat_rules"
+  value       = { for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : k => v.external_mapping }
 }
 output "vpn_gateway_nat_rules_internal_mapping" {
-  description = "List of internal_mapping values across all vpn_gateway_nat_rules"
-  value       = [for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : v.internal_mapping]
+  description = "Map of internal_mapping values across all vpn_gateway_nat_rules, keyed the same as var.vpn_gateway_nat_rules"
+  value       = { for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : k => v.internal_mapping }
 }
 output "vpn_gateway_nat_rules_ip_configuration_id" {
-  description = "List of ip_configuration_id values across all vpn_gateway_nat_rules"
-  value       = [for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : v.ip_configuration_id]
+  description = "Map of ip_configuration_id values across all vpn_gateway_nat_rules, keyed the same as var.vpn_gateway_nat_rules"
+  value       = { for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : k => v.ip_configuration_id }
 }
 output "vpn_gateway_nat_rules_mode" {
-  description = "List of mode values across all vpn_gateway_nat_rules"
-  value       = [for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : v.mode]
+  description = "Map of mode values across all vpn_gateway_nat_rules, keyed the same as var.vpn_gateway_nat_rules"
+  value       = { for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : k => v.mode }
 }
 output "vpn_gateway_nat_rules_name" {
-  description = "List of name values across all vpn_gateway_nat_rules"
-  value       = [for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : v.name]
+  description = "Map of name values across all vpn_gateway_nat_rules, keyed the same as var.vpn_gateway_nat_rules"
+  value       = { for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : k => v.name }
 }
 output "vpn_gateway_nat_rules_type" {
-  description = "List of type values across all vpn_gateway_nat_rules"
-  value       = [for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : v.type]
+  description = "Map of type values across all vpn_gateway_nat_rules, keyed the same as var.vpn_gateway_nat_rules"
+  value       = { for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : k => v.type }
 }
 output "vpn_gateway_nat_rules_vpn_gateway_id" {
-  description = "List of vpn_gateway_id values across all vpn_gateway_nat_rules"
-  value       = [for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : v.vpn_gateway_id]
+  description = "Map of vpn_gateway_id values across all vpn_gateway_nat_rules, keyed the same as var.vpn_gateway_nat_rules"
+  value       = { for k, v in azurerm_vpn_gateway_nat_rule.vpn_gateway_nat_rules : k => v.vpn_gateway_id }
 }
 
